84e3d4a6ba514c36019f2c875cf7994a79d19602
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-02-03 14:00:29 +0000

    Adding jquery, fickility and movie slider (not edited)

--- src/script.js
+++ src/script.js
@@ -1,15 +1,14 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+// function setup() {
+//   const allEpisodes = getAllEpisodes();
+//   makePageForEpisodes(allEpisodes);
+// }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+// function makePageForEpisodes(episodeList) {
+//   const rootElem = document.getElementById("root");
+//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+// }
 
-window.onload = setup;
+// window.onload = setup;
 
 //Header
 const navigation = document.getElementById("nav");
@@ -17,12 +16,11 @@ const menu = document.getElementById("menu");
 
 menu.addEventListener("click", () => {
   // The navigation.children.length means the following :-
-  // The children inside a parent are basically an array of elements; So, here I'm finding the length of the array aka how many children are inside the nav bar.
-  //   Yup That's all.
+  // The children inside a parent are basically an array of elements;
+  // So, here I'm finding the length of the array aka how many children are inside the nav bar.
   navigation.style.setProperty("--childenNumber", navigation.children.length);
 
   //    Casually Toggling Classes to make them animate on click
-  //   Regular stuff ;)
   navigation.classList.toggle("active");
   menu.classList.toggle("active");
 });
