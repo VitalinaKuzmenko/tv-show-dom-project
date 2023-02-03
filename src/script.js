26c87a15ea2cadb2b1fa15345e4e50e1e3874896
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-02-03 19:07:30 +0000

    Adding js function makePageForEpisodes() and working on css

--- src/script.js
+++ src/script.js
@@ -1,14 +1,41 @@
-// function setup() {
-//   const allEpisodes = getAllEpisodes();
-//   makePageForEpisodes(allEpisodes);
-// }
+function setup() {
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
+}
 
-// function makePageForEpisodes(episodeList) {
-//   const rootElem = document.getElementById("root");
-//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-// }
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  let episodes = document.createElement("div");
+  episodes.classList.add("episodes");
+  rootElem.appendChild(episodes);
+  //looping through episode list and displaying episode on webpage
+  episodeList.forEach((episodeFromList) => {
+    let episode = document.createElement("div");
+    episode.classList.add("episode");
+    episodes.appendChild(episode);
+    let img = document.createElement("img");
+    let title = document.createElement("h1");
+    let paragraph = document.createElement("p");
+    img.src = episodeFromList.image.medium;
+    title.innerHTML = `${createEpisodeName(
+      episodeFromList.season,
+      episodeFromList.number
+    )} - ${episodeFromList.name}`;
+    paragraph.innerHTML = episodeFromList.summary;
+    episode.appendChild(img);
+    episode.appendChild(title);
+    episode.appendChild(paragraph);
+  });
 
-// window.onload = setup;
+  function createEpisodeName(season, episode) {
+    if (season < 10) season = "0" + season;
+    if (episode < 10) episode = "0" + episode;
+
+    return `S${season}E${episode}`;
+  }
+}
+
+window.onload = setup;
 
 //Header
 const navigation = document.getElementById("nav");
