d4a8cae6b6bb425ef22209505e441dcd87dff482
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-02-03 20:26:10 +0000

    Creating a live search input inside js

--- src/script.js
+++ src/script.js
@@ -1,16 +1,17 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  createLiveSearch();
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  let episodes = document.createElement("div");
+  let episodes = document.createElement("ul");
   episodes.classList.add("episodes");
   rootElem.appendChild(episodes);
   //looping through episode list and displaying episode on webpage
   episodeList.forEach((episodeFromList) => {
-    let episode = document.createElement("div");
+    let episode = document.createElement("li");
     episode.classList.add("episode");
     episodes.appendChild(episode);
     let img = document.createElement("img");
@@ -26,13 +27,69 @@ function makePageForEpisodes(episodeList) {
     episode.appendChild(title);
     episode.appendChild(paragraph);
   });
+}
 
-  function createEpisodeName(season, episode) {
-    if (season < 10) season = "0" + season;
-    if (episode < 10) episode = "0" + episode;
+function createEpisodeName(season, episode) {
+  if (season < 10) season = "0" + season;
+  if (episode < 10) episode = "0" + episode;
 
-    return `S${season}E${episode}`;
-  }
+  return `S${season}E${episode}`;
+}
+
+function makeOneEpisode(episodeFromList) {
+  const episodes = document.querySelector(".episodes");
+  let episode = document.createElement("li");
+  episode.classList.add("episode");
+  episodes.appendChild(episode);
+  let img = document.createElement("img");
+  let title = document.createElement("h1");
+  let paragraph = document.createElement("p");
+  img.src = episodeFromList.image.medium;
+  title.innerHTML = `${createEpisodeName(
+    episodeFromList.season,
+    episodeFromList.number
+  )} - ${episodeFromList.name}`;
+  paragraph.innerHTML = episodeFromList.summary;
+  episode.appendChild(img);
+  episode.appendChild(title);
+  episode.appendChild(paragraph);
+}
+
+// create live search input for episodes
+function createLiveSearch() {
+  const filterSection = document.querySelector(".filter-menu");
+  const episodes = document.querySelector(".episodes");
+  const search = document.createElement("input");
+  search.classList.add("search-episode");
+  filterSection.appendChild(search);
+  search.setAttribute("autocomplete", "off");
+  search.setAttribute("type", "search");
+  search.setAttribute("id", "search");
+  search.setAttribute("placeholder", "Search for episode...");
+
+  const allEpisodes = getAllEpisodes();
+  let search_episode = "";
+
+  const showList = () => {
+    episodes.innerHTML = " ";
+    allEpisodes
+      .filter((episode) => {
+        console.log(episode.name);
+        return (
+          episode.summary.toLowerCase().includes(search_episode) ||
+          episode.name.toLowerCase().includes(search_episode)
+        );
+      })
+      .forEach((e) => {
+        makeOneEpisode(e);
+      });
+  };
+  showList();
+
+  search.addEventListener("input", (event) => {
+    search_episode = event.target.value.toLowerCase();
+    showList();
+  });
 }
 
 window.onload = setup;
